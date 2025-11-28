8622fe57964151d049d3d51346cf79e1c5894e75
lupete3 <lupeteplacide@gmail.com>
2025-11-28 11:54:03 +0200

    Fix Color

--- resources/views/components/page-header.blade.php
+++ resources/views/components/page-header.blade.php
@@ -1,6 +1,6 @@
 @props(['title'])
 
-<div class="container-fluid py-5" style="margin-bottom: 90px; background-color: #566886ff;">
+<div class="container-fluid py-5" style="margin-bottom: 90px; background-color: #a01f4cff;">
     <div class="row py-5">
         <div class="col-12 pt-lg-5 mt-lg-5 text-center">
             <h1 class="display-4 text-white animated zoomIn">{{ $title }}</h1>
